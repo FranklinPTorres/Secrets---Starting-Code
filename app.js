71c06b613d2923ac4239badb765d2564740302d7
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-06-08 17:41:34 +0100

    2 level of security encrepting, also gitingore and .env add it

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const { Console } = require("console");
+
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -12,10 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ( {
     email: String,
     password: String,
-};
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
