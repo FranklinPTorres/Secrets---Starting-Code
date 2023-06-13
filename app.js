2752e5ab0547be20a1ac3f7a6e7b7cfaed69423b
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-06-13 20:05:33 +0100

    adding passport, passport-local-mongoose, solving mongoose issues

--- app.js
+++ app.js
@@ -5,29 +5,45 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { Console } = require("console");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const session = require('express-session'); 
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: "Our little Secret!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
 
 const userSchema = new mongoose.Schema ( {
     email: String,
     password: String,
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home");
@@ -43,43 +59,11 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req, res){
 
-    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err){
-           if (err){
-            console.log(err);
-           } else {
-            res.render("secrets")
-           };
-        });
-    });
-   
 });
 
 
 app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser){
-        if(err) {
-            console.log(err);
-        } else{
-            if (foundUser){
-                bcrypt.compare(myPlaintextPassword, foundUser.password, function(err, result) {
-                   if (result === true){
-                    res.render("secrets")
-                   }
-                });
-                   
-          }
-        };
-    })
 });
 
 
